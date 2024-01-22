a902afabd8f0705b6db543148a6eea0920064bb8
Sudo <93739034+SudodoSu@users.noreply.github.com>
2024-01-23 00:22:02 +0100

    refactore

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,4 +1,5 @@
 import type { Config } from "tailwindcss";
+import defaultTheme from "tailwindcss/defaultTheme"
 
 const config: Config = {
   content: [
@@ -7,11 +8,50 @@ const config: Config = {
     "./app/**/*.{js,ts,jsx,tsx,mdx}",
   ],
   theme: {
+    container: {
+      padding: {
+        DEFAULT: '15px',
+      },
+      center: true,
+      
+      screens: {
+        sm: '36rem',
+        md: '42rem',
+        lg: '56rem',
+        xl: '72rem',
+      },
+    
+    },
     extend: {
       backgroundImage: {
         "gradient-radial": "radial-gradient(var(--tw-gradient-stops))",
         "gradient-conic":
           "conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))",
+      },
+      fontFamily: {
+        Barlow: ["var(--barlow-regular)", ...defaultTheme.fontFamily.sans],
+        Roboto: ["var(--roboto_regular)", ...defaultTheme.fontFamily.sans],
+      },
+      fontSize: {
+        biggest_font_size: "2rem",
+        normal_font_size: "0.938rem",
+        small_font_size: "0.813rem",
+        smaller_font_size: "0.75rem",
+      },
+      colors: {
+        clr_primary: "#fbb040",
+        clr_sec: "#ff0000",
+        primary_light: "rgba(251, 176, 64, 0.2)",
+        text_clr: "#e5e5e5",
+        white: "#ffffff",
+        btn_blue: "#40cefb",
+        black_clr: "#000000",
+        btn_clr: "#ffc438",
+        artic: "#ffc53a",
+        home_overlay: "rgba(251, 176, 64, 0.25)",
+        token_color: "rgba(46, 46, 46, 1)",
+        token_bg: "rgba(46, 46, 46, 1)",
+        body_clr: "#141414",
       },
     },
   },
